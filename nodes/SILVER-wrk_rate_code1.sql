7ce156568a5f7f22b2a4e301041f3fa1549566e9
Douglas Barrett <douglas.barrett@coalesce.io>
2026-07-13 13:15:46 -0600

    deleted 18 nodes

--- nodes/SILVER-wrk_rate_code1.sql
+++
@@ -1,6 +0,0 @@
-@id("67bfd6fb-aab8-435f-b309-824eb0e89e40")
-@nodeType("461")
-SELECT
-     "rate_code_id" AS "rate_code_id",
-     "rate_code"."rate_code" AS "rate_code"
-FROM {{ ref('BRONZE', 'rate_code') }} `rate_code`
